e2e168a554b9d21718dada6ad39b5700a0370d48
marinaoleinik <marina.oleinik@tthk.ee>
2024-02-01 16:10:36 +0200

    Add project files.

---
+++ TARpv22_Mobile/EntryPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARpv22_Mobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EntryPage : ContentPage
+	{
+        Label lbl;
+        Editor editor;
+        BoxView box;
+		public EntryPage ()
+		{
+            box = new BoxView
+            {
+                Color = Color.White,
+                CornerRadius = 10,
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+            };
+            //Ekraani suurus
+            int mon_height=(int)DeviceDisplay.MainDisplayInfo.Height;
+            int mon_width = (int)DeviceDisplay.MainDisplayInfo.Width;
+
+            Button Tagasi_btn = new Button
+            {
+                Text = "Tagasi Start lehele",
+                BackgroundColor = Color.FromRgb(32, 32, 34),
+                TextColor = Color.Fuchsia
+            };
+            lbl = new Label
+            { 
+                Text = "Mingi tekst",
+                BackgroundColor= Color.FromRgb(200, 32,0),
+                TextColor= Color.Fuchsia,
+            };
+            editor = new Editor
+            {
+                Placeholder = "Sisesta siia tekst...",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.FromRgb(32, 32, 255),
+                Children = {lbl,editor,box, Tagasi_btn},
+                VerticalOptions = LayoutOptions.End
+            };
+            
+            Content = st;
+            Tagasi_btn.Clicked += Tagasi_btn_Clicked;
+            editor.TextChanged += Editor_TextChanged;
+        }
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lbl.Text=editor.Text;
+        }
+
+        private async void Tagasi_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ TARpv22_Mobile/StartPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARpv22_Mobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentPage
+	{
+		public StartPage ()
+		{
+			Button Entry_btn = new Button
+			{
+				Text = "Entry leht",
+				BackgroundColor = Color.FromRgb(100, 32, 34),
+				TextColor=Color.Fuchsia
+			};
+            Button Time_btn = new Button
+            {
+                Text = "Time leht",
+                BackgroundColor = Color.FromRgb(100, 32, 34),
+                TextColor = Color.Fuchsia
+            };
+            StackLayout st = new StackLayout
+			{
+				Orientation = StackOrientation.Vertical,
+				BackgroundColor = Color.FromRgb(32, 32, 255)
+			};
+			st.Children.Add(Entry_btn);
+			st.Children.Add(Time_btn);
+			Content = st;
+            Entry_btn.Clicked += Entry_btn_Clicked;
+            Time_btn.Clicked += Time_btn_Clicked;
+
+		}
+
+        private async  void Time_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TimePage());
+        }
+
+        private async void Entry_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EntryPage());
+        }
+    }
+}
